840a828ed2af0971f57fe3090033a8f46578b0d0
dmitry-kzn <83679943+dmitry-kzn@users.noreply.github.com>
2022-11-02 20:28:59 +0100

    Create maven.sh

---
+++ maven.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/default-java
+export M2_HOME=~/install_apache_maven_demo/apache-maven-3.8.4
+export MAVEN_HOME=~/install_apache_maven_demo/apache-maven-3.8.4
+export PATH=${M2_HOME}/bin:${PATH}
